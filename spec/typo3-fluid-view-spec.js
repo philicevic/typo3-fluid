e3cb3f38a6ce840d80ebc68cb741aead77aa3a0e
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2019-03-12 02:43:09 +0100

    Cleanup repository

--- spec/typo3-fluid-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Typo3FluidView from '../lib/typo3-fluid-view';
-
-describe('Typo3FluidView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
